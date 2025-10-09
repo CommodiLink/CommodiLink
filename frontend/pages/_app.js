ff3d13d37d2143756002f5777bc656b920db4ac5
CommodiLink <contact@saltjets.com>
2025-10-09 16:19:13 +0100

    Update _app.js

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,3 +1,3 @@
-import '@/styles/globals.css'
-import Layout from '@/components/Layout'
-export default function App({Component,pageProps}){return(<Layout><Component {...pageProps}/></Layout>)}
+import '../styles/globals.css'
+import Layout from '../components/Layout'
+
