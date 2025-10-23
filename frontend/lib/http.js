737a09e91599d75e94fa978439d5dcbbf1194471
CommodiLink <contact@saltjets.com>
2025-10-23 16:38:06 +0100

    Update http.js

--- frontend/lib/http.js
+++ frontend/lib/http.js
@@ -5,7 +5,7 @@ const base = process.env.NEXT_PUBLIC_API_BASE_URL?.replace(/\/$/, "");
 
 const http = axios.create({
   baseURL: base || "",
-  withCredentials: true, // <- SEND COOKIES!
+  withCredentials: true,                // <-- required so cookies are sent
   headers: { "Content-Type": "application/json" },
 });
 
